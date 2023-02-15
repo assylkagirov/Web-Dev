b4cd937922df5a51ad73754d3aae42adbfa41126
Assylzhan Kagirov <asik.kagirov@gmail.com>
2023-02-15 10:41:42 +0600

    :fire: full 3 lab has been done :white_check_mark:

---
+++ lab3/stask/todo.js
@@ -0,0 +1,47 @@
+let tasks = document.querySelector('.tasks');
+let inputText = document.querySelector('.input-field');
+let addButton = document.querySelector('.add-button');
+
+let task = document.querySelectorAll('.task');
+let deleteImage = document.querySelectorAll('.delete-img');
+
+function addList(){
+    // console.log("ok")                    
+    if(inputText.value != null){
+        //create div of new tasks
+        let div = document.createElement("div");
+        div.className="task";
+
+        let label = document.createElement("label");
+        label.className = "task-new";
+        
+        let check = document.createElement("input");
+        check.type = "checkbox";
+        check.className = "task-checkbox";
+
+        let span = document.createElement("span");
+        span.innerHTML = inputText.value;
+
+        let image = document.createElement("img");
+        image.className = "delete-img";
+        image.src = "https://icons-for-free.com/iconfiles/png/512/delete+remove+trash+trash+bin+trash+can+icon-1320073117929397588.png";
+        image.addEventListener('click', remove);
+
+        tasks.appendChild(div);
+        div.appendChild(label);
+        label.appendChild(check);
+        label.appendChild(span);
+        div.appendChild(image);
+
+        inputText.value = null;
+    }
+}
+
+function remove(){
+    this.parentNode.remove();
+}
+
+function clearAll(){
+    deleteImage[0].addEventListener('click', remove);
+}
+clearAll();
