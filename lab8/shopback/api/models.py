e7e71d294fe5d6e91705d68080c04aef57093df8
Assylzhan Kagirov <98634686+assylkagirov@users.noreply.github.com>
2023-04-01 15:49:57 +0600

    first and second tasks has been done 🤡

---
+++ lab8/shopback/api/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+    name = models.CharField()
+    price = models.FloatField()
+    description = models.TextField()
+    count = models.IntegerField()
+    is_active = models.BooleanField()
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'price': self.price,
+            'description': self.description,
+            'count': self.count,
+            'is_active': self.is_active
+        }
+class Category(models.Model):
+    name = models.CharField()
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name
+        }
